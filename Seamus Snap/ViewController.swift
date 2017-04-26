5536c9f3a321fe96deae080de647334c5c3e5974
Seamus MacIsaac <seamus.macisaac@gmail.com>
2017-04-25 20:33:54 -0500

    Initial Commit

---
+++ Seamus Snap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Seamus Snap
+//
+//  Created by Seamus MacIsaac on 2017-04-25.
+//  Copyright © 2017 Seamus MacIsaac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
