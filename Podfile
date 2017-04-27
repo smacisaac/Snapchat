adf5961c4689ba324b91e6c56a0840009fecda91
Seamus MacIsaac <seamus.macisaac@gmail.com>
2017-04-27 12:49:55 -0500

    Login working

--- Podfile
+++ Podfile
@@ -7,5 +7,8 @@ target 'Seamus Snap' do
 
   # Pods for Seamus Snap
 pod 'Firebase/Core'
+pod 'Firebase/Auth'
+pod 'Firebase/Database'
+pod 'Firebase/Storage'
 
 end
--- Seamus Snap/SignInViewController.swift
+++ Seamus Snap/SignInViewController.swift
@@ -7,23 +7,48 @@
 //
 
 import UIKit
+import Firebase
+
 
 class SignInViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextField: UITextField!
     
     @IBOutlet weak var passwordTextField: UITextField!
     
-    @IBAction func turnUpTapped(_ sender: Any) {
-    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-   
-
-
-}
+    
+    @IBAction func turnUpTapped(_ sender: Any) {
+        FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+            print("We tried to sign in..")
+            if error != nil{
+                print("Hey we have an error:\(error)")
+                
+                FIRAuth.auth()?.createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user, error) in
+                    print("We tried to create a user")
+                    
+                    if error != nil{
+                        print("Hey we have an error:\(error)")
+                    }else{
+                        print("Created user successfully!")
+                        self.performSegue(withIdentifier: "signinsegue", sender:nil )
+                    }
+                    
+                })
+                
+            }else{
+                print("Signed in successfully")
+                self.performSegue(withIdentifier: "signinsegue", sender:nil )
+            }
+        })
+        
+    }//end turnUpTaped
+    
+    
+}// end class
 
