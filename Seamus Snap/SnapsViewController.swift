2900d8b90d5f63c196bed8d8b1402ba58ab187be
Seamus MacIsaac <seamus.macisaac@gmail.com>
2017-04-27 13:13:29 -0500

    login and logout working

---
+++ Seamus Snap/SnapsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SnapsViewController.swift
+//  Seamus Snap
+//
+//  Created by Seamus MacIsaac on 2017-04-27.
+//  Copyright © 2017 Seamus MacIsaac. All rights reserved.
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func logoutTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+   
+
+}
