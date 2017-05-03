818808689196ac1879dfc7eda80883678e50f680
Seamus MacIsaac <seamus.macisaac@gmail.com>
2017-05-03 12:54:31 -0600

    ffjfjf

---
+++ Seamus Snap/PictureViewController.swift
@@ -0,0 +1,59 @@
+//
+//  PictureViewController.swift
+//  Seamus Snap
+//
+//  Created by Seamus MacIsaac on 2017-05-02.
+//  Copyright © 2017 Seamus MacIsaac. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    var imagePicker = UIImagePickerController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    // ACTIONS
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .savedPhotosAlbum
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func nextTapped(_ sender: Any) {
+    }
+    
+    
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
